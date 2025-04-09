f64dfb8d4d1ed64895807e8c315fd6ac8be9e3db
pierasiqueira <pierasiqueira@gmail.com>
2025-04-09 19:46:25 -0300

    adicionando produto no carrinho

--- js/app.js
+++ js/app.js
@@ -3,16 +3,20 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-    let preco = quantidade.value * valorUnitario;
-    alert(preco);
+    let quantidade = document.getElementById('quantidade').value;
 
     // calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+  
     // adicionar no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+
     // atualizar o valor total
+
+
 }
 
 function limpar() {
